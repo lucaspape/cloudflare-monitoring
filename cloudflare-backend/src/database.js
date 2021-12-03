4a43e64c79f6fbef1eb2366811c1ebf65a2e53ef
Lucas Pape <admin@lucaspape.de>
2021-12-03 19:48:32 +0100

    Add database

---
+++ cloudflare-backend/src/database.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+
+const DBNAME = 'cloudflare';
+
+const mysqlConnection = mysql.createConnection({
+    host: 'mariadb',
+    user: 'root',
+    password: 'password',
+    database: DBNAME
+  });
+
+function createTables(callback){
+  const CREATE_LOG_TABLE = 'create table if not exists logs (timestamp TIMESTAMP,method TEXT, url TEXT, status INT, referer: TEXT, user_agent TEXT, protocol TEXT, domain TEXT, origin TEXT, path TEXT, hash TEXT, query TEXT, device_type TEXT, country TEXT, clientIP TEXT, duration INT, country_name TEXT);';
+
+  mysqlConnection.query(CREATE_LOG_TABLE, (err, result) => {
+    if(err){
+      callback(err);
+    }else{
+      callback();
+    }
+  })
+}
+
+function insertLog(log, callback){
+  const INSERT_LOG_QUERY = 'insert into `' + DBNAME + '`.`logs` (method, url, status, referer, user_agent, protocol, domain, origin, path, hash, query, device_type, country, clientIP, duration, country_name) values (' + mysqlConnection.escape(log.method) + ', ' + mysqlConnection.escape(log.url) + ', ' + mysqlConnection.escape(log.status) + ', ' + mysqlConnection.escape(log.referer) + ', ' + mysqlConnection.escape(log.user_agent) + ', ' + mysqlConnection.escape(log.protocol) + ', ' + mysqlConnection.escape(log.domain) + ', ' + mysqlConnection.escape(log.origin) + ', ' + mysqlConnection.escape(log.path) + ', ' + mysqlConnection.escape(log.hash) + ', ' + mysqlConnection.escape(log.query) + ', ' + mysqlConnection.escape(log.device_type) + ', ' + mysqlConnection.escape(log.country) + ', ' + mysqlConnection.escape(log.clientIP) + ', ' + mysqlConnection.escape(log.duration) + ', ' + mysqlConnection.escape(log.country_name) + ');';
+
+  mysqlConnection.query(INSERT_LOG_QUERY, (err, result) => {
+    if(err){
+      callback(err);
+    }else{
+      callback(undefined, result);
+    }
+  });
+}
+
+module.exports = {
+  connect: function(callback){
+    mysqlConnection.connect((err) => {
+      if(err){
+        callback(err);
+      }else{
+        createTables(callback);
+      }
+    });
+  },
+  insertLog: insertLog
+}
--- cloudflare-backend/src/index.js
+++ cloudflare-backend/src/index.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const database = require('database');
 
 const PORT = 80;
 
-const app = express();
+database.connect((err) => {
+  if(err){
+    console.log(err);
+  }else{
+    const app = express();
 
-app.use(cors());
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '5mb'
-}));
+    app.use(cors());
+    app.use(bodyParser.json({limit: '5mb'}));
+    app.use(bodyParser.urlencoded({
+      extended: true,
+      limit: '5mb'
+    }));
 
-app.post('/insert', (req, res) => {
-  console.log(JSON.stringify(req.body));
+    app.post('/insert', (req, res) => {
+      database.insertLog(req.body, (err, result) => {
+        if(err){
+          console.log(err);
+          res.status(500).send('failed to log');
+        }else{
+          res.send('OK');
+        }
+      });
+    });
 
-  res.send('OK');
-});
+    app.listen(PORT, () => {
+      console.log('Server started on port ' + PORT);
+    });
 
-app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT);
+  }
 });
