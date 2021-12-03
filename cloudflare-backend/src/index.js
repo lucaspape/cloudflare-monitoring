a2a3b12ac14419f31ff2962c6fc9f8608bdb05b8
Lucas Pape <admin@lucaspape.de>
2021-12-03 19:49:35 +0100

    Fix filename

--- cloudflare-backend/src/index.js
+++ cloudflare-backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const database = require('database.js');
+const database = require('./database.js');
 
 const PORT = 80;
 
